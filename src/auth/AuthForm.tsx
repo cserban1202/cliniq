83076fc6363c05cdeb881f766519de4b544ead3e
cserban1202 <cristiserban12@yahoo.com>
2023-03-23 19:13:44 +0200

    updated style

--- src/auth/AuthForm.tsx
+++ src/auth/AuthForm.tsx
@@ -7,36 +7,37 @@ import Button from "../Utils/Button";
 import { Link } from "react-router-dom";
 
 export default function AuthForm(props: authFormProps) {
-    return (
-        <Formik 
-        initialValues={props.model}
-        onSubmit={props.onSubmit}
-        validationSchema={Yup.object({
-            email: Yup.string().required("Required")
-                .email("Invalid email address"),
-            password: Yup.string().required("Required")
-        })}
-        >
-            {formikProps => (
-                <Form>
-                    <TextField 
-                    displayName="Email"
-                    field="email"/>
+  return (
+    <Formik
+      initialValues={props.model}
+      onSubmit={props.onSubmit}
+      validationSchema={Yup.object({
+        email: Yup.string().required("Required").email("Invalid email address"),
+        password: Yup.string().required("Required"),
+      })}
+    >
+      {(formikProps) => (
+        <Form>
+          <TextField displayName="Email" field="email" />
 
-                    <TextField 
-                    displayName="Password"
-                    field="password"
-                    type="password"/>
-
-                    <Button disabled={formikProps.isSubmitting} type="submit">Send</Button>
-                    <Link className = "btn btn-secondary" to="/">Cancel</Link>
-                </Form>
-            )}
-        </Formik>
-    )
+          <TextField displayName="Password" field="password" type="password" />
+          <div className="pt-5"></div>
+          <Button disabled={formikProps.isSubmitting} type="submit">
+            Send
+          </Button>
+          <Link className="btn btn-secondary" to="/">
+            Cancel
+          </Link>
+        </Form>
+      )}
+    </Formik>
+  );
 }
 
-interface authFormProps{
-    model: userCredentials;
-    onSubmit: (values: userCredentials, actions: FormikHelpers<userCredentials>)=> void;
+interface authFormProps {
+  model: userCredentials;
+  onSubmit: (
+    values: userCredentials,
+    actions: FormikHelpers<userCredentials>
+  ) => void;
 }
